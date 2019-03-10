d5f96e7c31317390163618aca53f8b75629d89cc
VBTX <jv.svedaite@gmail.com>
2019-03-10 18:48:34 +0100

    login routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
+
 	get '/login' do
 	erb :login	
+	end
+
+	post '/login' do
+	@user = User.find_by(email: params[:email])
+	if @user.authenticate(params[:password])
+		session[:user_id] = @user.id
+		redirect "/users/#{@user.id}"
+
+	else
+
+	end
+
+
+	get '/users/:id' do
+		"this will be the show route"
 	end
 end
