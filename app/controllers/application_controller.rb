24863bac5adf9d9e5fc8ae4fda6e78a5f1d275e3
VBTX <jv.svedaite@gmail.com>
2019-03-10 20:35:50 +0100

    add helper methods: logged_in and current)_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,24 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
-  end
+    enable  :sessions
+    set :session_secret, "glossaries_rock"
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do 
+
+  	def logged_in? 
+  		!!current_user
+  	end
+
+  	def current_user
+  		@current_user = User.find_by(id: session[:user_id])
+
+  	end
+  	
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,17 @@ class UsersController < ApplicationController
 
 	post '/login' do
 	@user = User.find_by(email: params[:email])
-	if @user.authenticate(params[:password])
+	if @user && @user.authenticate(params[:password])
 		session[:user_id] = @user.id
 		redirect "/users/#{@user.id}"
 
 	else
 
+		puts "Your login information is incorrect"
+		puts "If you don't have an account, please signup here:"
+
 	end
+end
 
 
 	get '/users/:id' do
