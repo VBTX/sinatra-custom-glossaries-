3ce329cd5d6d6f6f6122505792cb0779823ab808
VBTX <jv.svedaite@gmail.com>
2019-03-12 08:47:27 +0100

    check whether entry is valid and redirect if not

--- app/controllers/glossaries_controller.rb
+++ app/controllers/glossaries_controller.rb
@@ -3,7 +3,20 @@ class GlossariesController < ApplicationController
 		erb :'glossaries/new'
 	end
 
-	post '/glossaries' do 
+	post '/glossaries' do
+		if !logged_in?
+			redirect '/'
+		end
+
+
+		if params[:content] != ""
+			@glossary = Glossary.create(title: params[:title], user_id: current_user.id)
+			redirect "/glossaries/#{@glossary.id}"
+		else
+			redirect '/glossaries/new'
+		end
+
+
 	end
 
 	#show and index routes
