08d9510b49f779f52bab69a2533f6b46d6767d12
VBTX <jv.svedaite@gmail.com>
2019-03-12 06:01:41 +0100

    add signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,33 @@ class UsersController < ApplicationController
 
 	post '/login' do
 	@user = User.find_by(email: params[:email])
-	if @user && @user.authenticate(params[:password])
-		session[:user_id] = @user.id
-		redirect "/users/#{@user.id}"
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
+			redirect "/users/#{@user.id}"
 
-	else
+		else
 
-		puts "Your login information is incorrect"
-		puts "If you don't have an account, please signup here:"
+			puts "Your login information is incorrect"
+			puts "If you don't have an account, please signup here:"
+
+		end
+	end
+
+	get '/signup' do 
+		erb :signup
+	end
+
+	post '/users' do 
+		if params[:name] != "" && params[:email] != "" && params[:password] != ""
+			@user = User.create(params)
+			redirect '/users/#{@user.id}'
+		else
 
 	end
-end
 
 
 	get '/users/:id' do
-		"this will be the show route"
+		@user = User.find_by(id: params[:id])
+		erb :'/users/show'
 	end
 end
