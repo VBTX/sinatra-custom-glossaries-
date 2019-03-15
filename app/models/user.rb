c8aeb84687b8433fd381813bfd31c78d53327597
VBTX <jv.svedaite@gmail.com>
2019-03-15 19:16:31 +0100

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :email, uniqueness: true
 	has_many :glossaries
 	has_many :entries, through: :glossaries
 end
